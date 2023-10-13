0a5be96b49770f4c166c6a8e57ac39c533c0d66d
nahas@beaconinfo.tech <nahasay5@gmail.com>
2023-10-13 15:28:25 +0530

    dash

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,32 +4,96 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import styles from "./dashbaord.module.scss";
 import LayoutDash from "../../components/LayoutDash/page";
-import { Card } from "@mui/material";
+import { Box, Card } from "@mui/material";
+import Image from "next/image";
+import SimpleLineChart from "@/components/LineChartDash/page";
 
 const Dashboard = () => {
+  let videoSrc = "http://80.13.46.193:2503/mjpg/video.mjpg";
   return (
     <div>
-    <LayoutDash>
-    <Container>
-        <Grid container spacing={3}>
-          {/* Dashboard Widgets */}
-          <Grid item xs={12} sm={6} md={4}>
-            {/* Your first widget content */}
+      <LayoutDash>
+        <>
+          <Grid container spacing={3} p={2}>
+            <Grid item lg={6} xs={12}>
+              <Card>
+                <div className={styles.dFlex}>
+                  <Image
+                    src={videoSrc}
+                    alt=""
+                    width={500}
+                    height={300}
+                    unoptimized={true}
+                  />
+                  <Image
+                    src={videoSrc}
+                    alt=""
+                    width={500}
+                    height={300}
+                    unoptimized={true}
+                  />
+                  <Image
+                    src={videoSrc}
+                    alt=""
+                    width={500}
+                    height={300}
+                    unoptimized={true}
+                  />
+                  <Image
+                    src={videoSrc}
+                    alt=""
+                    width={500}
+                    height={300}
+                    unoptimized={true}
+                  />
+                </div>
+              </Card>
+              {/* Dashboard Widgets */}
+            </Grid>
 
-            <Card>
-            <video width="400"  src="mov_bbb.mp4" />
-            </Card>
+            <Grid item xs={12} lg={6} spacing={2}>
+              <Card sx={{ height: "48%", width: "99%" }}>
+                <SimpleLineChart />
+              </Card>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                <Card sx={{ marginTop: "2%", height: "50%", width: "49%" }}>
+                  <div className={styles.contentBar}>
+                    <h3> Analytics: </h3>
+                    <ul>
+                      <li> No of people</li>
+                      <li> No of Male</li>
+                      <li> No of Female</li>
+                    </ul>
+                  </div>
+                </Card>
+                <Card sx={{ marginTop: "2%", height: "50%", width: "49%" }}>
+                  <div className={styles.contentBar}>
+                    <h3> Open Alert: </h3>
+                    <ul>
+                      <li>
+                        <span className={styles.warning}></span> ALert One
+                      </li>
+                      <li>
+                        <span className={styles.danger}></span> ALert One
+                      </li>
+                      <li>
+                        <span className={styles.sucess}></span> ALert One
+                      </li>
+                    </ul>
+                  </div>
+                </Card>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            {/* Your second widget content */}
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            {/* Your third widget content */}
-          </Grid>
-        </Grid>
-      </Container>
-    </LayoutDash>
-   
+        </>
+      </LayoutDash>
     </div>
   );
 };
